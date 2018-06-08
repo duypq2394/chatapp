268c5e1aae4d60be58abb7df19cc38a1bc810288
pham_quang_duy <pham_quang_duy@mediado.local>
2018-06-08 16:08:43 +0900

    use passport for user_login

--- config/key.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI:
-    'mongodb://admin:admin123@ds151840.mlab.com:51840/chatapp'
-}
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+    'mongodb://admin:admin123@ds151840.mlab.com:51840/chatapp',
+    secretOrKey : 'secret'
+};
